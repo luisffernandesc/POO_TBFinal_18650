5fddaebd268ca4d31cc522ef36ab7da06c61a5cc
luisffernandesc <a18650@alunos.ipca.pt>
2024-10-28 18:52:25 +0000

    Add project files.

---
+++ Gestao_alojamentos_turísticos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourismManagementSystem
+{
+    // Main program entry
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Tourism Management System");
+            // You may call initialization or test functions here if needed
+        }
+    }
+}
